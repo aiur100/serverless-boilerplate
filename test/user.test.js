54bbed3aae6dfc1d7b40ef4404b24bd2064f7177
Christopher Hill <chill@redlineadvantage.com>
2022-02-18 15:59:05 -0500

    Added tests

--- test/user.test.js
+++ test/user.test.js
@@ -1,20 +1,26 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 require("dotenv").config();
+const assert = require("assert");
 
 describe("user management", function () {
+
+  const testUser = {
+   // userId: "1234",
+   userId:1,
+    name: "Christopher Hill",
+  }
+
   it("Create user", async function () {
     const response = await fetch(process.env.API_URL + "/users", {
       method: "POST",
-      body: JSON.stringify({
-        userId: "1234",
-        name: "Christopher Hill",
-      }),
+      body: JSON.stringify(testUser),
       headers: {
         "Content-Type": "application/json",
       },
     }).then((r) => r.json());
     console.log(response);
+    assert.strictEqual(!response.error,true,JSON.stringify(response));
   });
 
   it("Get users", async function () {
@@ -23,4 +29,15 @@ describe("user management", function () {
     );
     console.log(response);
   });
+
+  it("Delete user", async function(){
+    const response = await fetch(process.env.API_URL + "/users", {
+        method: "DELETE",
+        body: JSON.stringify(testUser),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((r) => r.json());
+      console.log(response);
+  })
 });
